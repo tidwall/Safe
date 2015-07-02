e2648330fcdba634aeac847582f17f0c25c9ceb8
Josh Baker <joshbaker77@gmail.com>
2015-07-02 12:23:37 -0700

    docs update

--- Generator/docs.sh
+++ Generator/docs.sh
@@ -5,11 +5,11 @@ cd $(dirname "${BASH_SOURCE[0]}")
 cd ..
 jazzy -g https://github.com/tidwall/Safe -o docsb --skip-undocumented -a "Josh Baker" -m "Safe" -u "http://github.com/tidwall"
 
-echo ".nav-group-name a[href=\"Extensions.html\"] { display: none; }" >> docs/css/jazzy.css
-echo ".nav-group-name a[href=\"Extensions.html\"] ~ ul { display: none; }" >> docs/css/jazzy.css
-printf "%s(\".nav-group-name a[href='Extensions.html']\").parent().hide()\n" "$" >> docs/js/jazzy.js
-printf "%s(\".nav-group-name a[href='../Extensions.html']\").parent().hide()\n" "$" >> docs/js/jazzy.js
-printf "%s(\"header .content-wrapper a[href='index.html']\").parent().html(\"<a href='index.html'>Safe Docs</a>\")\n" "$" >> docs/js/jazzy.js
+echo ".nav-group-name a[href=\"Extensions.html\"] { display: none; }" >> docsb/css/jazzy.css
+echo ".nav-group-name a[href=\"Extensions.html\"] ~ ul { display: none; }" >> docsb/css/jazzy.css
+printf "%s(\".nav-group-name a[href='Extensions.html']\").parent().hide()\n" "$" >> docsb/js/jazzy.js
+printf "%s(\".nav-group-name a[href='../Extensions.html']\").parent().hide()\n" "$" >> docsb/js/jazzy.js
+printf "%s(\"header .content-wrapper a[href='index.html']\").parent().html(\"<a href='index.html'>Safe Docs</a>\")\n" "$" >> docsb/js/jazzy.js
 
 git checkout gh-pages
 function cleanup {
@@ -19,5 +19,5 @@ function cleanup {
 trap cleanup EXIT
 rm -rf docs
 mv docsb docs
-git add --all
+git add docs/
 git commit -m "updated docs"
